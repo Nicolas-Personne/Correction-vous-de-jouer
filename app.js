db817b828eba393b9a8a4ee4fd8028260a4200bc
Nicolas <nicolas.personne@novei.fr>
2023-02-01 13:46:31 +0100

    Exercice 2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 new Vue({
 	el: "#app",
 	data: {
-		message: "Bonjour les bachelors",
+		message: "Bonjour les Bachelors",
+		laManu: "La Manu, l'école des métiers du numérique",
+		alternative: "Logo de l'école la Manu",
 	},
 });
