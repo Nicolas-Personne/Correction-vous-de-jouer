3a7b165d06688c2ce45b454b05701e880e72499e
Nicolas <nicolas.personne@novei.fr>
2023-02-01 14:03:06 +0100

    Exercice 4

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ new Vue({
 		message: "Bonjour les Bachelors",
 		laManu: "La Manu, l'école des métiers du numérique",
 		alternative: "Logo de l'école la Manu",
+		btnMessage: "Voir les coordonnées",
 		campusAM: {
 			campus: "La Manu, campus Amiens",
 			ville: "Amiens",
 			adresse: "70 Rue des jacobins",
 			telephone: "0000000000",
 			email: "contact-amiens@lamanu.fr",
+		},
+		isShow: false,
+	},
+	methods: {
+		toggleCoord: function () {
+			this.isShow = !this.isShow;
 		},
 	},
 });
